4c1c9a3080331a1b4e05ad9f6355d23588103245
Bedriyecaglayan <bedis730@gmail.com>
2021-02-06 20:00:32 +0300

    Add project files.

---
+++ Dortislem/Matematik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dortislem
+{
+    class Matematik
+    {
+        public void Topla (int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine("toplam : " + toplam);
+        }
+        public void Cikarma (int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 - sayi2;
+            Console.WriteLine("sonuç : " + sonuc);
+        }
+
+        public void Bolme (int sayi1, int sayi2)
+        {
+            float sonuc = sayi1 / sayi2;
+            Console.WriteLine("sonuc : " + sonuc);
+        }
+        
+        public void Carpma (int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 * sayi2;
+            Console.WriteLine(" sonuc : " + sonuc);
+        }
+    }
+}
---
+++ Dortislem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dortislem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematik matematik = new Matematik();
+            matematik.Topla(10, 20);
+            matematik.Cikarma(5, 3);
+            matematik.Bolme(200, 5);
+            matematik.Carpma(9, 7);
+        }
+    }
+}
